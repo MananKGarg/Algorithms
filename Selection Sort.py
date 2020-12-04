7bb347b709c0abf8ce922ecc1d86b59f6a07ba86
Manan Kumar Garg <62146744+MananKGarg@users.noreply.github.com>
2020-12-05 04:00:58 +0530

    Create Selection Sort.py

---
+++ Selection Sort.py
@@ -0,0 +1,18 @@
+def findSmallest(arr):  #returns index of smallest element in array
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+def selectionSort(arr): # Find the smallest element in the array and append it to a new one
+    sortedArr = []
+    for i in range(len(arr)):
+        smallest = findSmallest(arr)
+        sortedArr.append(arr.pop(smallest))
+    return sortedArr
+
+arr = [5,3,6,2,10]
+print(selectionSort(arr))
