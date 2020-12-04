3a27f428b7712568fd7dab58072686dd4d2dc733
Manan Kumar Garg <62146744+MananKGarg@users.noreply.github.com>
2020-12-04 20:10:49 +0530

    Create quicksort.py

---
+++ quicksort.py
@@ -0,0 +1,27 @@
+def partition(A, l, r):
+    pivot = A[l]
+    j = l
+    for i in range(l+1, r+1):
+        if A[i] <= pivot:
+            j = j + 1
+            temp = A[j]
+            A[j] = A[i]
+            A[i] = temp
+
+    temp1 = A[j]
+    A[j] = A[l]
+    A[l] = temp1
+
+    return A, j
+
+def quicksort(A, l, r):
+    if l>=r:
+        return A
+    else:
+        A, m = partition(A,l,r)
+        quicksort(A,l,m-1)
+        quicksort(A,m+1,r)
+    return A
+
+arr = [10, 80, 30, 60, 60, 60, 70]
+print(quicksort(arr, 0, len(arr)-1))
