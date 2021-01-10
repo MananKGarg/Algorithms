7922d2146e2574805746df7c67f42fb7ca146556
Manan Kumar Garg <62146744+MananKGarg@users.noreply.github.com>
2021-01-10 21:55:24 +0530

    Create Longest Common Subsequence.cpp

---
+++ Longest Common Subsequence.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int max(int a, int b){
+    if (a > b){
+        return a;
+    }
+    else{
+        return b;
+    }
+}
+
+int lcs(char *A, char *B){
+    int m = strlen(A);
+    int n = strlen(B);
+    
+    int lcs_matrix[n + 1][m + 1];
+    
+    for (int i{}; i <=n; i++){
+        for (int j{}; j <= m; j++){
+            if (i == 0 || j == 0){
+                lcs_matrix[i][j] = 0;
+            }
+            else if (B[i-1] == A[j-1]){
+                lcs_matrix[i][j] = lcs_matrix[i-1][j-1] + 1;
+            }
+            else{
+                lcs_matrix[i][j] = max(lcs_matrix[i-1][j],lcs_matrix[i][j-1]);
+            }
+        }
+        
+    }
+    return lcs_matrix[n][m];
+}
+
+int main()
+{
+    char A[] = "aggtab";
+    char B[] = "gxtxayb";
+    
+    cout << "lcs " << lcs(A,B);
+    
+
+    return 0;
+}
