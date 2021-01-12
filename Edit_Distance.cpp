16ba4548961b20634d28862fde45d4395d6a0954
Manan Kumar Garg <62146744+MananKGarg@users.noreply.github.com>
2021-01-12 11:04:35 +0530

    Update Edit_Distance.cpp

--- Edit_Distance.cpp
+++ Edit_Distance.cpp
@@ -2,7 +2,7 @@
 #include <string>
 using namespace std;
 
-int min(int x, int y, int z){    // returns minimum of 3 numbers
+int min(int x, int y, int z){                          // returns minimum of 3 numbers
     if (x <= y && x <= z){
         return x;
     }    
@@ -14,11 +14,11 @@ int min(int x, int y, int z){    // returns minimum of 3 numbers
     }
 }
 
-int edit_distance(string s, string t){
+int edit_distance(string s, string t){                // converting s to t
     int matrix[s.length() + 1][t.length() + 1];
     
     matrix[0][0] = 0;
-    for(int i = 1; i <= s.length(); i++){
+    for(int i = 1; i <= s.length(); i++){             // edit distance between 1 character and 0 character is 1
         matrix[i][0] = 1;
     }
     for(int j = 1; j <= t.length(); j++){
@@ -27,7 +27,7 @@ int edit_distance(string s, string t){
     
     for (int i = 1; i <= s.length(); i++){
         for (int j = 1; j <= t.length(); j++){
-            if (s[i-1] == t[j-1]){
+            if (s[i-1] == t[j-1]){                    // if the last character is same, no additional step is required
                 matrix[i][j] = matrix[i-1][j-1];
             }
             else{
@@ -35,10 +35,9 @@ int edit_distance(string s, string t){
                 int insertion{};
                 int deletion{};
                 
-                replace = 1 + matrix[i-1][j-1];
-                insertion = 1 + matrix[i-1][j];
-                deletion = 1 + matrix[i][j-1];
-                
+                replace = 1 + matrix[i-1][j-1];       // replace the last character of s with t
+                insertion = 1 + matrix[i-1][j];       // insert a character at the end of t
+                deletion = 1 + matrix[i][j-1];        // delete a character at the end of t                
                 matrix[i][j] = min(replace, insertion, deletion);
             }
         }
